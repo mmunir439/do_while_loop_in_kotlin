0d87bf7967743cfc05b590b272835c682ddcecd3
Muhammad Munir <hrcoder3@gmail.com>
2025-10-18 12:33:35 +0500

    do while loop

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+//do while loop. it will executes once before checking the condition.
+// then it executes untill the condition will be true in here
+
+fun dowhile(){
+    var x=1;
+    do{
+        x++;
+        println("hi muhammad munir")
+    }while(x<3)
+}
+fun main(){
+    dowhile();
+}
